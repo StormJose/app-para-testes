dfd0bec400823f283c4ea5f5d94071dcec7cfc5f
stormJose <ricardo23feitosa@gmail.com>
2024-05-14 22:21:44 -0300

    [fix] Dropdown da barra de navegação

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,15 @@
 // static/js/script.js
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Code to run after the document is loaded
-    console.log("Website loaded!");
+  // Code to run after the document is loaded
+  console.log("Website loaded!");
 
-    // Example of event listener for a button (make sure to add your own selectors)
-    const someButton = document.querySelector("#some-button");
-    if (someButton) {
-        someButton.addEventListener("click", function() {
-            alert("Button clicked!");
-        });
-    }
+  // Example of event listener for a button (make sure to add your own selectors)
+  const someButton = document.querySelector("#some-button");
+  if (someButton) {
+    someButton.addEventListener("click", function () {
+      alert("Button clicked!");
+    });
+  }
 });
 
-// You can expand this file with more complex logic or API calls as needed
